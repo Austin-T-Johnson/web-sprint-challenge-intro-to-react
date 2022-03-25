6d7379791a14758510c35dcbc7b2d3de0fe88fac
Austin Johnson <austintjohnson92@gmail.com>
2022-03-24 19:15:11 -0700

    Added all names to the DOM

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledP = styled.p`
+color:white;
+`
+
+
+const Character = props => {
+   return (
+       <div>
+           <StyledP>{props.chars.name}</StyledP>
+       </div>
+   )
+
+}
+
+export default Character;
